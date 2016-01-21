8778983a9a2d8a8120c39c710a23ebed154d15ad
Carlo Quinonez <carlo@studiofathom.com>
2016-01-20 23:34:10 -0800

    Initial fork to create custom script for FATHOM build.

---
+++ base/scripts/lib/fathom-testbuild.sh
@@ -0,0 +1,24 @@
+# forked from ./build_app.sh
+
+set -e
+
+COPIED_APP_PATH=/copied-app
+BUNDLE_DIR=/tmp/bundle-dir
+
+# sometimes, directly copied folder cause some wierd issues
+# this fixes that
+cp -R /app $COPIED_APP_PATH
+cd $COPIED_APP_PATH
+
+meteor build $BUNDLE_DIR
+
+cd $BUNDLE_DIR/bundle/programs/server/
+npm i
+
+mv $BUNDLE_DIR/bundle /built_app
+
+# cleanup
+rm -rf $COPIED_APP_PATH
+rm -rf $BUNDLE_DIR
+rm -rf ~/.meteor
+rm /usr/local/bin/meteor
